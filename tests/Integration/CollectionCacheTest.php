f16177e8b4e032bc6371760bb9994d07240896fb
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2020-07-12 14:47:18 +0200

    add caching to collection

---
+++ tests/Integration/CollectionCacheTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace MageOne\Qps\Test\Integration;
+
+use MageOne\Qps\Test\AbstractTest;
+
+class CollectionCacheTest extends AbstractTest
+{
+    public function testCachingIsUsed()
+    {
+        $this->markTestIncomplete('Implement me');
+    }
+}

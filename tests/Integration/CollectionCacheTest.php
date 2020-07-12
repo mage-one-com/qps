fccf0a58d12533b5f26a7db31251a189eb0b9c2f
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2020-07-12 18:57:35 +0200

    Implement test to make sure caching is used and filed

--- tests/Integration/CollectionCacheTest.php
+++ tests/Integration/CollectionCacheTest.php
@@ -4,10 +4,26 @@ namespace MageOne\Qps\Test\Integration;
 
 use MageOne\Qps\Test\AbstractTest;
 
+/**
+ * @covers Mageone_Qps_Model_Resource_Rule_Collection
+ */
 class CollectionCacheTest extends AbstractTest
 {
     public function testCachingIsUsed()
     {
-        $this->markTestIncomplete('Implement me');
+        $this->assertTrue(
+            \Mage::app()->useCache('collections'),
+            'Collection cache needs to be enabled to run this test.'
+        );
+
+        \Mage::getResourceModel('qps/rule_collection')->load();
+        /** @var \Mageone_Qps_Model_Resource_Rule_Collection $collection */
+        $collection = \Mage::getResourceModel('qps/rule_collection')->load();
+
+        $refMethod = new \ReflectionMethod($collection, '_loadCache');
+        $refMethod->setAccessible(true);
+        $cache = $refMethod->invoke($collection, $collection->getSelect());
+
+        $this->assertNotEmpty($cache);
     }
 }

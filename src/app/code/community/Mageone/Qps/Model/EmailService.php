075d2f1a958e15466737e10d37d54492efddec77
Rico Neitzel <rico@buro71a.de>
2020-10-01 12:28:57 +0200

    validate email address before sending, additional logging

--- src/app/code/community/Mageone/Qps/Model/EmailService.php
+++ src/app/code/community/Mageone/Qps/Model/EmailService.php
@@ -7,19 +7,36 @@ class Mageone_Qps_Model_EmailService
 
     public function sendNotificationEmail(Mageone_Qps_Helper_Data $helper)
     {
+        if ($helper->isNotificationEnabled() === false ) {
+            return;
+        }
+
+        if(!$this->isEmailValid($helper->getNotifactionEmail())){
+            Mage::log('QPS notification email address seems to be invalid. Please check your configuration!');
+            return;
+        }
+        
         $variables = [];
         if (!$helper->isRuleAutoEnable()) {
             $variables['notautoenable'] = 'true';
         }
 
         $mail = Mage::getModel('core/email_template');
-        $mail->sendTransactional(
-            'mageone_qps_ruleupdate',
-            'general',
-            $helper->getNotifactionEmail(),
-            'Mage One QPS',
-            $variables
-        );
+        try {
+            $mail->sendTransactional(
+                'mageone_qps_ruleupdate',
+                'general',
+                $helper->getNotifactionEmail(),
+                'Mage One QPS',
+                $variables
+            );
+        } catch (Mage_Core_Exception $e) {
+            Mage::log('QPS notification email could not be send.');
+        }
+    }
 
+    private function isEmailValid($emailAddress)
+    {
+        return filter_var($emailAddress, FILTER_VALIDATE_EMAIL);
     }
 }
